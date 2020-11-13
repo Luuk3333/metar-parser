2f7de3cdcf265c93eb3d30940534326699396f47
Luuk3333 <Luuk3333@users.noreply.github.com>
2020-11-13 17:38:37 +0100

    Move data from dict to variables; Add getters; Update sample code;

--- metar_parser/Metar.py
+++ metar_parser/Metar.py
@@ -4,41 +4,63 @@ import json
 from fractions import Fraction
 
 class Report:
-    def find(self, regex, string):
-        matches = re.search(regex, string)
-        if not matches:
-            return None
-        return matches.group().strip()
+    """A parsed METAR report."""
 
-    # Try converting to int, if fails, return string
-    def int_or_str(self, string):
+    def __int_or_str(self, string):
+        """Try converting to int, if fails, return string."""
+        if not string:
+            return None
+
+        string = string.strip()
+
+        if '/' in string:
+            return None
+
         try:
             return int(string)
         except ValueError:
-            return string.strip()
+            return string
 
-    def __init__(self, report):
-        report = report.strip()
+    def __init__(self, raw):
+        """Parse an input METAR report.
 
-        self.data = {
-            'raw': report,
-            'decoded': False
-        }
+        Parameters
+        ----------
+        raw : str
+          Input METAR report.
+        """
+        self.raw = raw.strip()  # Input METAR report
+        self.parsed = False
+        self.ident = None
+        self.reported = None
+        self.report_modifier = None
+
+        self.wind_direction = None
+        self.wind_speed = None
+        self.wind_speed_unit = None
+        self.wind_gust = None
+        self.wind_variable_direction = None
+
+        self.temperature = None
+        self.dew_point = None
+
+        self.visibility_distance = None
+        self.visibility_distance_unit = None
 
         # Split report into main parts: ident, date+time, body, remarks
-        parts = re.match(r'^(\S{4})\s*(.*?Z)(.*?)(?:RMK(.*))?$', report)    # https://regex101.com/r/Nq5xhk/1
+        parts = re.match(r'^(\S{4})\s*(.*?Z)(.*?)(?:RMK(.*))?$', self.raw)  # https://regex101.com/r/Nq5xhk/1
 
         if not parts:
             return
 
-        self.data['ident'] = parts.group(1)
+        self.ident = parts.group(1)
 
         # Get date and time
         if parts.group(2):
             try:
                 dt = datetime.strptime(parts.group(2), '%d%H%M%z')
                 dt = dt.replace(year=datetime.today().year, month=datetime.today().month)
-                self.data['reported'] = dt.isoformat()
+                self.reported = dt.isoformat()
             except ValueError as e:
                 return
 
@@ -46,44 +68,38 @@ class Report:
             # Get wind data
             wind = re.search(r'\s(?:([\d\/]{3}|VRB)([\d\/]{2,3}))(?:G(\d{2,3}))?(KT|MPS)(?:\s(\d{3})V(\d{3})\s)?', parts.group(3))  # https://regex101.com/r/wuyEsI/3
             if wind:
-                obj = dict()
-
                 # Add main wind data
-                for index, key in enumerate(['direction', 'speed', 'gust']):
-                    value = wind.group(index+1)
+                self.wind_direction = self.__int_or_str(wind.group(1))
+                self.wind_speed = self.__int_or_str(wind.group(2))
+                self.wind_gust = self.__int_or_str(wind.group(3))
 
-                    if value and not '/' in value:
-                        obj[key] = self.int_or_str(value)
-
-                if 'speed' in obj:
-                    obj['speed_unit'] = wind.group(4).lower()
+                if self.wind_speed:
+                    self.wind_speed_unit = wind.group(4).lower()
 
                 # Add variable wind direction
-                if wind.group(5) and wind.group(6) and not ('/' in wind.group(5) or '/' in wind.group(6)):
-                    obj['variable_direction'] = [self.int_or_str(wind.group(5)), self.int_or_str(wind.group(6))]
+                variable_direction = [
+                    self.__int_or_str(wind.group(5)),
+                    self.__int_or_str(wind.group(6))
+                ]
 
-                if len(obj) > 0:
-                    self.data['wind'] = obj
+                if variable_direction[0] and variable_direction[1]:
+                    self.wind_variable_direction = variable_direction
 
             # Get temperature data
             temps = re.search(r'\s(M?\d{2})/(M?\d{2})', parts.group(3))    # https://regex101.com/r/oqplsG/1
             if temps:
-                obj = dict()
+                temperature = temps.group(1).replace('M', '-')
+                self.temperature = self.__int_or_str(temperature)
 
-                for index, key in enumerate(['temperature', 'dew_point']):
-                    value = temps.group(index+1).replace('M', '-')
-                    if value and not '/' in value:
-                        obj[key] = self.int_or_str(value)
-
-                if len(obj) > 0:
-                    self.data['temperatures'] = obj
+                dew_point = temps.group(2).replace('M', '-')
+                self.dew_point = self.__int_or_str(dew_point)
 
             # Get report modifier
             for modifier in ['AUTO', 'COR']:    
                 # Note: In case both AUTO and COR are present, COR will be used as per https://www.ofcm.gov/publications/fmh/FMH1/FMH1.pdf
                 # (p. 58: "In the event of a corrected METAR or SPECI, the report modifier, COR, shall be substituted in place of AUTO")
                 if modifier in parts.group(3):
-                    self.data['report_modifier'] = modifier
+                    self.report_modifier = modifier
 
             # Get visibility data
             visibility = re.search(r'\s(?:(\d{4})\s|(M)?([\d\s/]{1,5})(SM)|(CAVOK))', parts.group(3))   # https://regex101.com/r/YWZkDI/2/
@@ -111,21 +127,61 @@ class Report:
                     if visibility.group(4):
                         unit = visibility.group(4)
 
-                obj = dict()
+                self.visibility_distance = distance
+                self.visibility_distance_unit = unit
 
-                if distance:
-                    obj['distance'] = distance
+        self.parsed = True
 
-                    if unit:
-                        obj['distance_unit'] = unit
 
-                if len(obj) > 0:
-                    self.data['visibility'] = obj
+    def wind(self):
+        """Return the parsed wind data."""
+        return {
+            'wind_direction': self.wind_direction,
+            'wind_speed': self.wind_speed,
+            'wind_speed_unit': self.wind_speed_unit,
+            'wind_gust': self.wind_gust,
+            'wind_variable_direction': self.wind_variable_direction
+        }
 
-        self.data['decoded'] = True
+    def temperatures(self):
+        """Return the parsed temperature data."""
+        return {
+            'temperature': self.temperature,
+            'dew_point': self.dew_point
+        }
+
+    def visibility(self):
+        """Return the parsed visibility data."""
+        return {
+            'distance': self.visibility_distance,
+            'distance_unit': self.visibility_distance_unit
+        }
+
+    def result(self):
+        """Return the parsed report."""
+        return {
+            'raw': self.raw,
+            'parsed': self.parsed,
+            'ident': self.ident,
+            'reported': self.reported,
+            'report_modifier': self.report_modifier,
+            'wind': self.wind(),
+            'temperatures': self.temperatures(),
+            'visibility': self.visibility()
+        }
 
     def json(self):
-        return json.dumps(self.data, indent=4, sort_keys=True)
+        """Return the parsed report as JSON."""
+        return json.dumps(self.result(), indent=4, sort_keys=True)
 
-    def decoded(self):
-        return self.data['decoded']
+    def is_parsed(self):
+        """Indicate if the report could be parsed."""
+        return self.parsed
+
+    def get_raw(self):
+        """Return the raw METAR input."""
+        return self.raw
+
+    def get_ident(self):
+        """Return the weather station identifier."""
+        return self.ident
--- sample.py
+++ sample.py
@@ -2,11 +2,27 @@ from metar_parser import Metar
 
 reports = [
     "EHAM 020825Z 21022KT 190V250 9999 FEW008 SCT018 BKN022 17/15 Q1002 NOSIG",
-    "TNCB 020755Z AUTO 11004KT 9999 NCD 26/25 Q1008",
-    "KLAX 020753Z 05004KT 0SM R25L/1800V3000FT FG VV002 17/16 A3006 RMK AO2 SLP177 T01670161 402440128 $",
-    "EGPK 020920Z 25010G21KT 8000 -RA FEW014 SCT020 BKN042 09/08 Q0991"
+    "EGPK 020920Z 25010G21KT 8000 -RA FEW014 SCT020 BKN042 09/08 Q0991",
+    "PAUN 131256Z AUTO 08006KT 10SM SCT021 M04/M05 A2931 RMK AO2 SNE15 SLP927 P0000 T10391050 FZRANO",
 ]
 
-for raw in reports:
-    report = Metar.Report(raw)
+report = Metar.Report(reports[0])
+if report.is_parsed:
     print(report.json())
+
+print('--------')
+
+report = Metar.Report(reports[1])
+if report.is_parsed:
+    visibility = report.visibility()
+    print(report.get_raw())
+    print('Visibility: {} {}'.format(visibility['distance'], visibility['distance_unit']))
+
+print('--------')
+
+report = Metar.Report(reports[2])
+if report.is_parsed:
+    temps = report.temperatures()
+    print(report.get_raw())
+    print('Temperature: {}°C'.format(temps['temperature']))
+    print('Dew point: {}°C'.format(temps['dew_point']))
